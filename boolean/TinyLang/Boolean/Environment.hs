21182aad0969f14eb0d0be3fa829c6b85125976f
effectfully <effectfully@gmail.com>
2019-04-09 15:44:31 +0300

    Added a few instances for 'Env'

--- boolean/TinyLang/Boolean/Environment.hs
+++ boolean/TinyLang/Boolean/Environment.hs
@@ -12,7 +12,7 @@ import qualified Data.IntMap.Strict as IntMap
 -- | A simple representation of environments as 'IntMap's mapping variables to values
 newtype Env a = Env
     { unEnv :: IntMap a
-    }
+    } deriving (Show, Eq, Functor)
 
 lookupUnique :: Unique -> Env a -> a
 lookupUnique (Unique ind) (Env env) = env IntMap.! ind
