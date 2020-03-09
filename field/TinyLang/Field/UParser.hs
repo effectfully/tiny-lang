a3c69b0baa18373c44349bff532b53fa92dc09cf
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-03-09 14:04:14 +0100

    Remove redundant import to Control.Applicative.Combinators

--- field/TinyLang/Field/UParser.hs
+++ field/TinyLang/Field/UParser.hs
@@ -141,7 +141,6 @@ statements ::=
 
 module TinyLang.Field.UParser where
 
-import           Control.Applicative.Combinators hiding (many, sepBy, some)
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer      as L
