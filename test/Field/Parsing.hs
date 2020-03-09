0a58a2ab0d151b5fdca76795a1d832421de46d8a
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-03-09 10:32:01 +0100

    Start testing untyped parser
    For the time being I am disabling all other tests

---
+++ test/Field/Parsing.hs
@@ -0,0 +1,15 @@
+{- A simple parser test
+-}
+
+module Field.Parsing
+  ( test_parsing
+  ) where
+
+import TinyLang.Field.UParser
+import Test.Tasty
+
+test_parsing :: TestTree
+test_parsing =
+  testGroup "parsing"
+  [
+  ]
--- test/Main.hs
+++ test/Main.hs
@@ -1,18 +1,25 @@
 module Main where
 
-import qualified Boolean.Textual as Boolean (test_printerParserRoundtrip)
-import qualified Field.Axioms    as Field (test_axiomsExamples)
-import qualified Field.Textual   as Field (test_textual)
+-- import qualified Boolean.Textual as Boolean (test_printerParserRoundtrip)
+-- import qualified Field.Axioms    as Field (test_axiomsExamples)
+-- import qualified Field.Textual   as Field (test_textual)
+import qualified Field.Parsing   as Parsing (test_parsing)
 
 import           Test.Tasty
+
+-- test_all :: TestTree
+-- test_all =
+--     testGroup "all"
+--         [ Boolean.test_printerParserRoundtrip
+--         , Field.test_axiomsExamples
+--         , Field.test_textual
+--         ]
 
 test_all :: TestTree
 test_all =
     testGroup "all"
-        [ Boolean.test_printerParserRoundtrip
-        , Field.test_axiomsExamples
-        , Field.test_textual
-        ]
+    [ Parsing.test_parsing
+    ]
 
 main :: IO ()
 main = defaultMain test_all
