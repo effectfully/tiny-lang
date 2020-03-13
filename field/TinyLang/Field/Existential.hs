8ecdf89c0519649c08604c8353d9d1fe4d9a262b
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-03-13 22:11:03 +0100

    Start refactoring Uni out

---
+++ field/TinyLang/Field/Existential.hs
@@ -0,0 +1,22 @@
+module TinyLang.Field.Existential
+    ( Some (..)
+    , SomeOf (..)
+    , Forget (..)
+    , traverseSomeOf
+    ) where
+
+data Some (f :: k -> *) = forall x. Some (f x)
+data SomeOf uni (f :: k -> *) = forall x. SomeOf (uni x) (f x)
+
+class Forget some where
+    forget :: (forall x. f x -> r) -> some f -> r
+
+instance Forget Some where
+    forget f (Some a) = f a
+
+instance Forget (SomeOf uni) where
+    forget f (SomeOf _ a) = f a
+
+traverseSomeOf :: Functor m => (forall a. f a -> m (f a)) -> SomeOf uni f -> m (SomeOf uni f)
+traverseSomeOf f (SomeOf uni a) = SomeOf uni <$> f a
+
