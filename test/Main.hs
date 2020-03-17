86405a559085296755c3b00b5da8d717922f1001
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-03-17 12:03:42 +0100

    Re-enable old test-suite

--- test/Main.hs
+++ test/Main.hs
@@ -1,30 +1,28 @@
 module Main where
 
--- import qualified Boolean.Textual as Boolean (test_printerParserRoundtrip)
--- import qualified Field.Axioms    as Field (test_axiomsExamples)
--- import qualified Field.Textual   as Field (test_textual)
-import qualified Field.Raw.Textual   as Field (gen_test_parsing)
+import qualified Boolean.Textual   as Boolean (test_printerParserRoundtrip)
+import qualified Field.Axioms      as Field (test_axiomsExamples)
+import qualified Field.Textual     as Field (test_textual)
+import qualified Field.Raw.Textual as Raw (gen_test_parsing)
 
 import           Test.Tasty
 
--- test_all :: TestTree
--- test_all =
---     testGroup "all"
---         [ Boolean.test_printerParserRoundtrip
---         , Field.test_axiomsExamples
---         , Field.test_textual
---         ]
-
--- test_all :: TestTree
--- test_all =
---     testGroup "all"
---     [ Parsing.test_parsing
---     ]
+test_all :: TestTree
+test_all =
+    testGroup "all"
+        [ Boolean.test_printerParserRoundtrip
+        , Field.test_axiomsExamples
+        , Field.test_textual
+        ]
 
 main :: IO ()
 main =
     do
-        test_parsing <- Field.gen_test_parsing
-        
-        defaultMain $ testGroup "all" [ test_parsing
-                                      ]
+        test_rawParsing <- Raw.gen_test_parsing
+        defaultMain $
+            testGroup "all"
+                [ Boolean.test_printerParserRoundtrip
+                , Field.test_axiomsExamples
+                , Field.test_textual
+                , test_rawParsing
+                ]
