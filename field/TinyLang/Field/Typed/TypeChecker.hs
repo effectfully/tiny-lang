9e5c763af510825cb3f7fe70a9885398a7fbb6fa
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-03-25 11:06:58 +0100

    Clean up errors and API

--- field/TinyLang/Field/Typed/TypeChecker.hs
+++ field/TinyLang/Field/Typed/TypeChecker.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-| TypeChecker
 
 Potential resources:
@@ -22,8 +23,12 @@ import qualified TinyLang.Field.Typed.Core  as T
 import qualified TinyLang.Field.Raw.Core    as R
 
 import           Data.Kind
+import qualified Data.String.Interpolate    as QQ
 
-type MonadTypeError   m = MonadError String m
+{-| Utility Type Aliases or Constraints
+-}
+type TypeCheckError     = String
+type MonadTypeError   m = MonadError TypeCheckError m
 type MonadTypeChecker m = ( MonadSupply m
                           , MonadScope m
                           , MonadTypeError m
@@ -35,13 +40,15 @@ newtype TypeCheckerT e (m :: Type -> Type) a =
     TypeChecker { runTypeCheckerT :: (ExceptT e (StateT Scope (SupplyT m))) a }
     deriving newtype (Monad, Functor, Applicative, MonadError e, MonadSupply, MonadScope)
 
-{-|
+
+
+{-| A simple type checker
 -}
-type TypeChecker = TypeCheckerT String Identity
+type TypeChecker = TypeCheckerT TypeCheckError Identity
 
 {-|
 -}
-runTypeChecker :: TypeChecker a -> Either String a
+runTypeChecker :: TypeChecker a -> Either TypeCheckError a
 runTypeChecker typeChecker =
     runIdentity $ runSupplyT
                 $ evalStateT (runExceptT (runTypeCheckerT typeChecker))
@@ -51,15 +58,15 @@ runTypeChecker typeChecker =
 -}
 typeCheck
     :: forall f. (TextField f)
-    => R.Expr R.Var f -> TypeChecker (T.SomeUniExpr f)
-typeCheck = inferExpr
+    => R.Expr R.Var f -> Either TypeCheckError (T.SomeUniExpr f)
+typeCheck = runTypeChecker . inferExpr
 
 {-|
 -}
 checkType
     :: forall f a. (TextField f, KnownUni f a)
-    => R.Expr R.Var f -> TypeChecker (T.Expr f a)
-checkType = checkExpr (knownUni @f @a)
+    => R.Expr R.Var f -> Either TypeCheckError (T.Expr f a)
+checkType = runTypeChecker . checkExpr (knownUni @f @a)
 
 {-| 
 -}
@@ -168,7 +175,8 @@ checkUniVar
     :: forall m f a. (MonadTypeChecker m) => Uni f a -> R.Var -> m (T.UniVar f a)
 checkUniVar uni iden = do
     Some uniVar@(T.UniVar varUni _) <- inferUniVar iden
-    withGeqUniM uni varUni "universe mismatch" uniVar
+    let uniMismatch = typeMismatch uniVar uni varUni
+    withGeqUniM uni varUni uniMismatch uniVar
 
 {-|
 -}
@@ -180,7 +188,8 @@ checkExpr uni (R.EIf l m n) = do
     pure $ T.EIf tL tM tN
 checkExpr uni m = do
     SomeOf mUni tM <- inferExpr m
-    withGeqUniM uni mUni "universe mismatch" $ tM
+    let uniMismatch = typeMismatch tM uni mUni
+    withGeqUniM uni mUni uniMismatch $ tM
 
 {-|
 -}
@@ -206,3 +215,10 @@ unrollLoop var lower bound stats = do
     let env = insertVar var (Some $ fromInteger i) mempty
         report err = error $ "Panic: " ++ show err
     map (either report id . instStatement env) stats
+
+{-|
+-}
+
+typeMismatch :: forall a b c. (Show a, Show b, Show c)  => a -> b -> c -> TypeCheckError
+typeMismatch expr expected found =
+    [QQ.i|error:  Universe mismatch for expression #{show expr}\n  Expected:  #{show expected}\n    Found:  #{show found}|]
