2fc7d58f1e00df4bccc5fbf8e3d96875238556d1
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-03-26 13:09:50 +0100

    Start golden testing the type checker

---
+++ test/Field/TestUtils.hs
@@ -0,0 +1,47 @@
+module Field.TestUtils
+    ( testFiles
+    , goldenFile
+    , parseRational
+    , parseFilePath
+    , discoverTests
+    ) where
+
+import TinyLang.Prelude
+
+import TinyLang.ParseUtils
+import TinyLang.Field.Raw.Core (RawExpr)
+import TinyLang.Field.Raw.Parser
+
+import System.FilePath
+import System.FilePath.Glob
+import Test.Tasty
+import Test.Tasty.HUnit
+
+testFileExt :: String
+testFileExt = ".field"
+
+testFiles :: FilePath -> IO [FilePath]
+testFiles testDir = sort <$> globDir1 pat testDir
+    where pat = compile $ "*" ++ testFileExt
+
+goldenFile :: FilePath -> FilePath
+goldenFile filePath = dropExtension filePath ++ ".golden"
+
+parseRational :: String -> String -> Either String (RawExpr Rational)
+parseRational fileName str = parseString pTop fileName str
+
+parseFilePath :: FilePath -> IO (Either String (RawExpr Rational))
+parseFilePath filePath =
+    parseRational fileName <$> readFile filePath
+    where
+        fileName = takeFileName filePath
+
+discoverTests :: String -> FilePath -> (FilePath -> TestTree) -> IO TestTree
+discoverTests groupName testDir genTest = do
+    files <- testFiles testDir
+    let testFoundAnyFiles =
+            testCase "test discovery" $
+            not (null files) @? "didn't find any " ++ testFileExt ++  " files in " ++ testDir
+    let testCases = testFoundAnyFiles : map genTest files
+    pure $ testGroup groupName $ testCases 
+    
---
+++ test/Field/Typed/Textual.hs
@@ -0,0 +1,30 @@
+module Field.Typed.Textual
+    ( gen_test_typechecking
+    ) where
+
+import TinyLang.Field.Typed.Core (SomeUniExpr)
+import TinyLang.Field.Typed.TypeChecker
+import Field.TestUtils
+
+import Data.String
+import System.FilePath
+import Test.Tasty
+import Test.Tasty.Golden
+
+testDir :: FilePath
+testDir = "test" </> "Field" </> "Typed" </> "golden"
+
+typeCheckFilePath :: FilePath -> IO (Either String (SomeUniExpr Rational))
+typeCheckFilePath filePath = do
+    parsed <- parseFilePath filePath
+    pure $ typeCheck =<< parsed
+    
+genTest :: FilePath -> TestTree
+genTest filePath = goldenVsString name golden action
+    where name = takeBaseName filePath
+          golden = goldenFile filePath
+          action = fromString <$> either id show <$> typeCheckFilePath filePath
+
+gen_test_typechecking :: IO TestTree
+gen_test_typechecking =
+    discoverTests "type checking" testDir genTest
