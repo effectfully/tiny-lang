16b875d897eba37b4b765fac3888daef0e2bc987
Jakub Zalewski <jakub.zalewski@iohk.io>
2020-08-11 23:10:59 +0200

    Sketch the traversals for Typed

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,44 +1,103 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import           TinyLang.Field.Generator  ()
 import           TinyLang.Field.Typed.Core
 
+import           Control.Lens
 import           Control.Monad
 import           Data.List
+import           Data.Semigroup
 import           Test.QuickCheck
 
--- A couple of functions for checking the output of generators
-progNodes :: Program f -> Int
-progNodes = stmtsNodes . _programStatements
 
-stmtsNodes :: Statements f -> Int
-stmtsNodes = sum . map stmtNodes . unStatements
+box :: (KnownUni f a) => Expr f a -> SomeUniExpr f
+box expr = SomeOf knownUni expr
 
-stmtNodes :: Statement f -> Int
-stmtNodes (ELet _ e)         = 1 + exprNodes e
-stmtNodes (EAssert e)        = 1 + exprNodes e
+unbox :: forall f a. (KnownUni f a) => SomeUniExpr f -> Expr f a
+unbox (SomeOf uni expr) = withGeqUni uni (knownUni :: Uni f a) (error message) expr where
+    message = "Uni mismatch!"
 
-exprNodes :: Expr f a -> Int
-exprNodes (EConst _)          = 1
-exprNodes (EVar   _)          = 1
-exprNodes (EAppUnOp _ e)      = 1 + exprNodes e
-exprNodes (EAppBinOp _ e1 e2) = 1 + exprNodes e1 + exprNodes e2
-exprNodes (EIf e e1 e2)       = 1 + exprNodes e + exprNodes e1 + exprNodes e2
+boxF :: (KnownUni f a, Functor t) => t (Expr f a) -> t (SomeUniExpr f)
+boxF = fmap box
 
-progDepth :: Program f -> Int
+unboxF :: (KnownUni f a, Functor t) => t (SomeUniExpr f) -> t (Expr f a)
+unboxF = fmap unbox
+
+wrapF :: (KnownUni f a, KnownUni f b, Functor t) => (SomeUniExpr f -> t (SomeUniExpr f)) -> Expr f a -> t (Expr f b)
+wrapF f = unboxF . f . box
+
+progSubStatement :: Traversal' (Program f) (Statement f)
+progSubStatement f = \case
+    Program exts stmts ->
+        Program exts <$> stmtsSubStatement f stmts
+
+stmtsSubStatement :: Traversal' (Statements f) (Statement f)
+stmtsSubStatement f = \case
+    Statements stmts ->
+        Statements <$> traverse f stmts
+
+progSubExpr :: Traversal' (Program f) (SomeUniExpr f)
+progSubExpr f = \case
+    Program exts stmts ->
+        Program exts <$> stmtsSubExpr f stmts
+
+stmtsSubExpr :: Traversal' (Statements f) (SomeUniExpr f)
+stmtsSubExpr f = \case
+    Statements stmts ->
+        Statements <$> traverse (stmtSubExpr f) stmts
+
+stmtSubExpr :: forall f. Traversal' (Statement f) (SomeUniExpr f)
+stmtSubExpr f = \case
+    ELet uniVar@(UniVar uni _) expr ->
+        withKnownUni uni $
+            ELet uniVar <$> wrapF f expr
+    EAssert expr ->
+        EAssert <$> wrapF f expr
+
+exprSubExpr :: Traversal' (SomeUniExpr f) (SomeUniExpr f)
+exprSubExpr f = \case
+    SomeOf uni e0 -> SomeOf uni <$> case e0 of
+        EAppUnOp unOp e ->
+            withKnownUni (uniOfExpr e) $
+            EAppUnOp unOp <$> wrapF f e
+        EAppBinOp binOp e1 e2 ->
+            withKnownUni (uniOfExpr e1) $
+            withKnownUni (uniOfExpr e2) $
+                EAppBinOp binOp <$> wrapF f e1 <*> wrapF f e2
+        EIf e e1 e2 ->
+            withKnownUni (uniOfExpr e)  $
+            withKnownUni (uniOfExpr e1) $
+            withKnownUni (uniOfExpr e2) $
+                EIf <$> wrapF f e <*> wrapF f e1 <*> wrapF f e2
+        x -> pure x
+
+-- TODO:  Count external variables as well
+progNodes :: Program f -> Sum Int
+progNodes prog = foldMapOf progSubStatement stmtNodes prog
+
+stmtsNodes :: Statements f -> Sum Int
+stmtsNodes stmts = foldMapOf stmtsSubStatement stmtNodes stmts
+
+stmtNodes :: Statement f -> Sum Int
+stmtNodes stmt = Sum 1 <> foldMapOf stmtSubExpr exprNodes stmt
+
+exprNodes :: SomeUniExpr f -> Sum Int
+exprNodes e = Sum 1 <> foldMapOf exprSubExpr exprNodes e
+
+-- NOTE:  We need Max 0 for the empty case
+progDepth :: Program f -> Max Int
 progDepth = stmtsDepth . _programStatements
 
-stmtsDepth :: Statements f -> Int
-stmtsDepth = maximum . (0:) . map stmtDepth . unStatements
+stmtsDepth :: Statements f -> Max Int
+stmtsDepth stmts = Max 0 <> foldMapOf stmtsSubStatement stmtDepth stmts
 
-stmtDepth :: Statement f -> Int
-stmtDepth (ELet _ e)         = 1 + exprDepth e
-stmtDepth (EAssert e)        = 1 + exprDepth e
+stmtDepth :: Statement f -> Max Int
+stmtDepth stmt = (+1) <$> Max 0 <> foldMapOf stmtSubExpr exprDepth stmt
 
-exprDepth :: Expr f a -> Int
-exprDepth (EConst _)          = 1
-exprDepth (EVar _)            = 1
-exprDepth (EAppUnOp _ e)      = 1 + exprDepth e
-exprDepth (EAppBinOp _ e1 e2) = 1 + max (exprDepth e1) (exprDepth e2)
-exprDepth (EIf e e1 e2)       = 1 + max (exprDepth e)  (max (exprDepth e1) (exprDepth e2))
+exprDepth :: SomeUniExpr f -> Max Int
+exprDepth expr = (+1) <$> Max 0 <>  foldMapOf exprSubExpr exprDepth expr
 
 data TestResult = TestResult { nodes :: Int
                              , depth :: Int
@@ -48,7 +107,7 @@ data TestResult = TestResult { nodes :: Int
 runGen :: Int -> IO TestResult
 runGen size = do
     prog <- generate (resize size arbitrary) :: IO (Program (AField Rational))
-    pure $ TestResult (progNodes prog) (progDepth prog)
+    pure $ TestResult (getSum (progNodes prog)) (getMax (progDepth prog))
 
 average :: (Real a, Fractional b) => [a] -> b
 average xs = realToFrac (sum xs) / genericLength xs
